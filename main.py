1eaa12024d6c8a7b47a7c4c600663cc1237ec688
edubruma <carlosvieiramadeira@gmail.com>
2024-05-21 21:05:09 -0300

    versão 1.1.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+# imports as bibliotecas
+import os
+import time
+
 # usuário informe um número
 contador = int(input('Informe um número inteiro'))
+os.system('cls')
 
 # conta a partir os números informado até 0
 
 while contador >= 0:
-    print(contador)
+    print(f'Contagem regressiva: {contador}...')
+    time.sleep(1)
+    os.system('cls')
     contador -= 1
+print('BOOM!!!')
